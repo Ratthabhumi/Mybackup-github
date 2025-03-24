e7881895ce376c6f27e80d4d23d944abc1c71b16
Ratthabhumi <ratthaphumi@gmail.com>
2025-03-24 21:14:22 +0700

    Last

--- client/src/components/DoctorList.jsx
+++ client/src/components/DoctorList.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Calendar.css';
 
-const DoctorList = ({doctor}) => {
-	const navigate = useNavigate()
+const DoctorList = ({ doctor }) => {
+  const navigate = useNavigate();
   return (
-	<div>
-		<div className="card" onClick={() => navigate(`/doctor/appointment/${doctor._id}`)} style={{cursor: 'pointer'}}>
-			<div className="card-header">
-				Dr. {doctor.firstName} {doctor.lastName}
-			</div>
-			<div className="card-body">
-				<p><b>Specialization:</b> {doctor.specialization}</p>
-				<p><b>phone:</b> {doctor.phone}</p>
-				<p><b>email:</b> {doctor.email}</p>
-				<p><b>Available Timeslot:</b> {doctor.time[0]} - {doctor.time[1]}</p>
-		</div>
-	</div>
-	</div>
-  )
-}
+    <div>
+      <div className="card" onClick={() => navigate(`/doctor/appointment/${doctor._id}`)}>
+        <div className="card-header">
+          Dr. {doctor.firstName} {doctor.lastName}
+        </div>
+        <div className="card-body">
+          <p><b>Specialization:</b> {doctor.specialization}</p>
+          <p><b>Phone:</b> {doctor.phone}</p>
+          <p><b>Email:</b> {doctor.email}</p>
+          <p><b>Available Timeslot:</b> {doctor.time[0]} - {doctor.time[1]}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DoctorList
+export default DoctorList;
